75372ae873038a3acce61e5ce397c74c512c76bc
Cameron King <cking@ckxng.com>
2015-05-20 15:30:54 -0500

    Print site stats from Enjin

--- src/main/java/com/ckxng/enjinbenefits/EnjinBenefitsMod.java
+++ src/main/java/com/ckxng/enjinbenefits/EnjinBenefitsMod.java
@@ -9,6 +9,9 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
+import java.net.*;
+import java.io.*;
+import org.json.*;
 
 @Mod(modid = EnjinBenefitsMod.MODID, version = EnjinBenefitsMod.VERSION, name = EnjinBenefitsMod.NAME)
 
@@ -22,10 +25,10 @@ public class EnjinBenefitsMod {
     public static final String VERSION = "0.1.0";
 
     /** The key provided by the Enjin admin panel - SECRET! */
-    public static String enjinKey = "";
+    public static String enjinApiKey = "";
     
     /** The API URL provided by the Enjin admin panel */
-    public static String enjinApiUrl = "https://example.enjin.com/api";
+    public static String enjinApiUrl = "https://example.enjin.com/api/v1/api.php";
     
     /**
      * 
@@ -36,10 +39,10 @@ public class EnjinBenefitsMod {
             Configuration config = new Configuration(event.getSuggestedConfigurationFile());
             config.load();
 
-            enjinKey = config.getString("enjinKey", "api", enjinKey, 
+            enjinApiKey = config.getString("key", "api", enjinApiKey, 
             		"The key provided by the Enjin admin panel - SECRET!");
 
-            enjinApiUrl = config.getString("enjinApiUrl", "api", enjinApiUrl, 
+            enjinApiUrl = config.getString("url", "api", enjinApiUrl, 
             		"The API URL provided by the Enjin admin panel");
             
             config.save();
@@ -50,6 +53,42 @@ public class EnjinBenefitsMod {
     public void init(FMLInitializationEvent event)
     {
         System.out.println("Let's have fun with: " + this.NAME);
+        
+        try {
+        	// generate stats API payload
+        	JSONObject enjinStatsPayload = new JSONObject()
+        			.put("jsonrpc","2.0")
+        			.put("id", (int)(Math.random()*1000))
+        			.put("params", new JSONObject().put("api_key",enjinApiKey))
+        			.put("method", "Stats.get");
+        	
+	        // connect to collect stats
+	        URL enjinStatsUrl = new URL(enjinApiUrl);
+	        HttpURLConnection enjinStatsConn = (HttpURLConnection)enjinStatsUrl.openConnection();
+	        enjinStatsConn.setRequestMethod("POST");
+	        enjinStatsConn.setDoOutput(true);
+	        enjinStatsConn.setRequestProperty("Content-Type", "application/json");
+	        
+	        // send stats payload
+	        System.out.println("Sending request: "+enjinStatsPayload.toString());
+	        OutputStreamWriter out = new OutputStreamWriter(
+	        		enjinStatsConn.getOutputStream());
+	        out.write(enjinStatsPayload.toString());
+	        out.flush();
+	        
+	        // read stats
+	        BufferedReader in = new BufferedReader(
+	        		new InputStreamReader(
+	        				enjinStatsConn.getInputStream()));
+	        String lineIn;
+	        while((lineIn = in.readLine()) != null) {
+	        	System.out.println(lineIn);
+	        }
+	        in.close();
+        } catch(Exception ex) {
+        		System.err.println("Unable to fetch Enjin Stats from "+enjinApiUrl);
+        }
+        
     }
 }
 
